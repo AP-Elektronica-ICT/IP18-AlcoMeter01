6645f58ba1edfc7bd4ce0c5c8854c733173efbcd
Elke <elke.renard@gmail.com>
2018-04-24 18:26:09 +0200

    bluetooth stuurt simulatiemeting door

--- Alco/src/pages/main/main.ts
+++ Alco/src/pages/main/main.ts
@@ -3,6 +3,7 @@ import { IonicPage, NavController, NavParams, MenuController, AlertController } 
 import { AuthenticatieProvider } from '../../providers/authenticatie/authenticatie';
 import { FirebaseProvider } from '../../providers/firebase/firebase';
 import { BluetoothProvider } from '../../providers/bluetooth/bluetooth';
+import { BluetoothSerial } from '@ionic-native/bluetooth-serial';
 import { Chart } from 'chart.js';
 import { CallNumber } from '@ionic-native/call-number';
 import * as math from 'mathjs';
@@ -21,7 +22,7 @@ import * as math from 'mathjs';
 export class MainPage {
 
 
-  public meting: any;
+  public meting: number;
   public beschrijving:String = "Een lekkere mojito!";
   resultaat  = [];
   donutChart:any;
@@ -30,7 +31,7 @@ export class MainPage {
   emergency: any;
   private country:any;
 
-  constructor(public alertCtrl: AlertController, private menuCtrl: MenuController, public fb: FirebaseProvider,private Noodnummer: CallNumber, public navCtrl: NavController, public navParams: NavParams, public afAuth: AuthenticatieProvider, public bt: BluetoothProvider) {
+  constructor(public bluetoothSerial: BluetoothSerial, public alertCtrl: AlertController, private menuCtrl: MenuController, public fb: FirebaseProvider,private Noodnummer: CallNumber, public navCtrl: NavController, public navParams: NavParams, public afAuth: AuthenticatieProvider, public bt: BluetoothProvider) {
     this.menuCtrl.enable(true, 'menu');
     /*this.fb.getSettings().then(data => {
       this.emergency.toString =  data.emergencyNumber;
@@ -60,10 +61,9 @@ export class MainPage {
 
   public onButtonClick() {
     
-    this.receiveData();
-    this.meting = math.round(math.random(0, 1.5),2);
-    this.resultaat= [this.meting, this.beschrijving];
-    this.fb.saveMeasurement(this.resultaat);
+    this.receiveData()
+    //this.meting = math.round(math.random(0, 1.5),2);
+
     
     this.donutChart = new Chart(document.getElementById('donutChart'), {
       
@@ -101,10 +101,15 @@ export class MainPage {
   
     }
 
-  private receiveData(){
-    this.bt.receiveData();
-    this.meting = this.bt.meting;
-  }
+    public receiveData(){
+      this.bluetoothSerial.readUntil("f").then(data => { 
+        console.log(data); 
+        this.meting = Number(data.substring(0, (data.length-1))); 
+        console.log(this.meting); this.bluetoothSerial.clear();    
+        this.resultaat= [this.meting, this.beschrijving];
+        this.fb.saveMeasurement(this.resultaat);
+      });
+    }
 
   private color(){
     if(this.meting < this.maxPromille)
